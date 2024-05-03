2b7d27f8b593d88636620e9a80d724e7a421a151
FerbaDev <elferbarron1@gmail.com>
2024-05-03 00:33:57 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Arquitectura por capas
+
+inicia proyecto --------------------------------- 00:08  
+juguetes json en compas mongo db ---------------- 00:28  
+conectando mongo db ----------------------------- 00:30  
+juguetes router --------------------------------- 00:33
+
+
+
+dejé en ------- 00:24
+(req, res) => {}
+() => {}
---
+++ src/app.js
@@ -0,0 +1,20 @@
+import express from "express";
+
+
+const app = express(); 
+const PUERTO = 8080;
+import "./database.js"; 
+import juguetesRouter from "./routes/juguetes.router.js";
+
+
+//Middleware 
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static("./src/public"));
+
+//rutas
+app.use("/juguetes", juguetesRouter)
+//listen
+app.listen(PUERTO, () => {
+    console.log(`Conectado a http://localhost:${PUERTO}`);
+})
